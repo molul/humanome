61081a2f57fa5f7dfb4d64cba7acedc62ff3dafa
molul <luismorcillo@gmail.com>
2024-11-02 06:07:07 +0100

    build: added nuxt icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/icon']
   // primevue: {
   // options: {
   //   theme: {
