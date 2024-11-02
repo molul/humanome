fd19f5a1b4a19552d79d619efc7fd36fe61d1e46
molul <luismorcillo@gmail.com>
2024-11-02 21:28:13 +0100

    feat: added head meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import Metronome from '~/components/Metronome.vue'
+
+useSeoMeta({
+  title: `Humanome - The humanized metronome`,
+  ogTitle: `Humanome - The humanized metronome`,
+  description:
+    'A metronome that slightly changes tempo from time to time to simulate human behaviour',
+  ogDescription:
+    'A metronome that slightly changes tempo from time to time to simulate human behaviour'
+  // ogImage: 'https://example.com/image.png',
+  // twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
