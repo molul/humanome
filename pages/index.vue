dc153020d51fdd8a9b2a8f91e68e7892a38ad386
molul <luismorcillo@gmail.com>
2024-11-02 23:32:00 +0100

    feat: added author info

--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,19 @@ useSeoMeta({
 
 <template>
   <div
-    class="p-4 lg:p-20 bg-secondary-50 dark:bg-secondary-900 text-secondary-700 dark:text-secondary-200 h-screen-dvh flex justify-center transition-colors"
+    class="p-4 lg:p-20 bg-secondary-50 dark:bg-secondary-900 text-secondary-700 dark:text-secondary-200 h-screen-dvh flex justify-center transition-colors text-xs lg_text-sm"
   >
-    <Metronome />
+    <div
+      class="w-full lg:max-w-sm m-auto flex flex-col gap-4 bg-white dark:bg-secondary-800 p-6 rounded-lg shadow-lg border border-secondary-300 dark:border-secondary-500"
+    >
+      <Metronome />
+      <div class="text-xs text-center font-medium">
+        Developed by
+        <NuxtLink class="textOnly" to="https://www.luismorcilloluque.com" target="_blank">
+          Luis Morcillo Luque
+        </NuxtLink>
+        with Nuxt 3 &copy; 2024
+      </div>
+    </div>
   </div>
 </template>
