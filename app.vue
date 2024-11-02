abab30b68bf39a41a7ef0dc36774350c22b33099
molul <luismorcillo@gmail.com>
2024-11-02 21:16:23 +0100

    style: added dark mode

--- app.vue
+++ app.vue
@@ -8,12 +8,5 @@
 </template>
 
 <style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.15s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-}
+@import '~/assets/css/theme.css';
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@ import Metronome from '~/components/Metronome.vue'
 </script>
 
 <template>
-  <div class="p-4 lg:p-20 bg-secondary-50 h-screen-dvh flex justify-center">
+  <div
+    class="p-4 lg:p-20 bg-secondary-50 dark:bg-secondary-900 text-secondary-700 dark:text-secondary-200 h-screen-dvh flex justify-center transition-colors"
+  >
     <Metronome />
   </div>
 </template>
