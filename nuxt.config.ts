34705aaca2dce13a0e1bd4cfb7f791eccd40d887
molul <luismorcillo@gmail.com>
2024-11-02 21:16:39 +0100

    build: added Nuxt Color Mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/icon']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode'],
+
+  icon: {
+    clientBundle: {
+      icons: ['mdi:metronome', 'solar:moon-bold', 'solar:sun-2-bold'],
+      // scan all components in the project and include icons
+      scan: false,
+
+      // guard for uncompressed bundle size, will fail the build if exceeds
+      sizeLimitKb: 1100
+    }
+  },
+
+  colorMode: {
+    classSuffix: '',
+    storageKey: 'theme'
+  }
+
   // primevue: {
   // options: {
   //   theme: {
